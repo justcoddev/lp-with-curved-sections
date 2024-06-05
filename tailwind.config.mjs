6b6d75b8b872c06a9f5dc9882cb4d18064e1257d
justcoddev <edisonpaulcrz@hotmail.com>
2024-06-05 12:06:28 -0500

    extend fonts & colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,22 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+
+			fontFamily: {
+				"heading": ['"Poppins"', "sans-serif"],
+				"body": ['"Open Sans"', "serif"],
+			},
+
+			colors: {
+				"p-pink": "hsl(322, 100%, 66%)",
+				"p-light-pink": "hsl(321, 100 %, 78 %)",
+				"p-light-red": "hsl(0, 100 %, 63 %)",
+
+				"n-very-dark-cyan": "hsl(192, 100 %, 9 %)",
+				"n-very-pale-blue": "hsl(207, 100 %, 98 %)"
+			}
+		},
 	},
 	plugins: [],
 }
