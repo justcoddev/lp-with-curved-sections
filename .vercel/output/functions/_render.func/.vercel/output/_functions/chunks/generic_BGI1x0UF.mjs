65009f07e6641e6bf5b54ff3499714975f1c2bec
justcoddev <edisonpaulcrz@hotmail.com>
2024-06-06 14:30:59 -0500

    feat: Update image paths for header and page sections

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/generic_BGI1x0UF.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/generic_Pb5hLUVp.mjs');
+
+export { page };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/chunks/index_CE943BsK.mjs
@@ -0,0 +1,5 @@
+export { renderers } from '../renderers.mjs';
+
+const page = () => import('./pages/index_B8Pwew05.mjs');
+
+export { page };
